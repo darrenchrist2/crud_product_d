15fdd35732e62603a419b7c457f1478e68b5926b
salvestacia <darrensiwibu@gmail.com>
2025-02-24 18:08:27 +0700

    penambahan file controller user

---
+++ rest_api_project/src/Controller/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = new User();
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'User registered successfully'], 201);
+    }
+
+    #[Route('/login', name: 'login', methods: ['POST'])]
+    public function login(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->findOneBy(['username' => $data['username']]);
+
+        if (!$user || $user->getPassword() !== $data['password']) {
+            return new JsonResponse(['message' => 'Invalid credentials'], 401);
+        }
+
+        return new JsonResponse(['message' => 'Login successful']);
+    }
+}
